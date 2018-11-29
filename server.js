3a80af93ec1083e89cb7ba35e0fa8f46ce7b3c73
wuxiz <wuxizhazhahui@example.com>
2018-11-29 21:14:01 +0800

    注册and用户分页

--- server.js
+++ server.js
@@ -54,12 +54,122 @@ app.post('/api/login', function (req, res) {
 
 //注册的请求
 app.post('/api/register', function (req, res) {
-
+    var name = req.body.username;
+    var pwd = req.body.password;
+    var nickname = req.body.nickname;
+    var sex = req.body.sex;
+    var age = parseInt(req.body.age);
+    var isAdmin = req.body.isAdmin === '是' ? true : false;
+    var results = {};
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
+        if (err) {
+            results.code = -1;
+            results.msg = '数据库连接失败';
+            res.json(results);
+            return;
+        }
+        var db = client.db('project');
+        async.series([
+            function (cb) {
+                db.collection('user').find({ username: name }).count(function (err, num) {
+                    if (err) {
+                        cb(err)
+                    } else if (num > 0) {
+                        // 用户名已存在
+                        cb(new Error('用户名已存在'));
+                    } else {
+                        // 用户名不存在,可以注册
+                        cb(null);
+                    }
+                })
+            },
+            function (cb) {
+                db.collection('user').insertOne({
+                    username: name,
+                    password: pwd,
+                    nickname: nickname,
+                    age: age,
+                    sex: sex,
+                    isAdmin: isAdmin
+                }, function (err) {
+                    if (err) {
+                        cb(err);
+                    } else {
+                        cb(null);
+                    }
+                })
+            }
+        ], function (err, result) {
+            if (err) {
+                //注册失败
+                results.code = -1;
+                results.msg = '用户名已存在';
+            } else {
+                //注册成功
+                results.code = 0;
+                results.msg = '注册成功';
+            }
+            // 不管成功or失败，关闭
+            client.close();
+            res.json(results);
+        })
+    })
 })
 
 //用户列表
 app.get('/api/user/list', function (req, res) {
+    var page = parseInt(req.query.page);
+    var pageSize = parseInt(req.query.pageSize);
+    var totalSize = 0;
+    var totalPage = 0;
+    var results = {};
+    MongoClient.connect(url, { useNewUrlParser: true }, function (err, client) {
+        if (err) {
+            results.code = -1;
+            results.msg = '数据库连接失败';
+            res.json(results);
+            return;
+        }
+        var db = client.db('project');
 
+        async.series([
+            function (cb) {
+                db.collection('user').find().count(function (err, num) {
+                    if (err) {
+                        cb(err);
+                    } else {
+                        totalSize = num;
+                        cb(null);
+                    }
+                })
+            },
+            function (cb) {
+                db.collection('user').find().limit(pageSize).skip(page * pageSize - pageSize).toArray(function (err, data) {
+                    if (err) {
+                        cb(err);
+                    } else {
+                        cb(null, data);
+                    }
+                })
+            }
+        ], function (err, result) {
+            if (err) {
+                results.code = -1;
+                results.msg = err.message;
+            } else {
+                totalPage = Math.ceil(totalSize / pageSize);
+                results.code = 0;
+                results.msg = '查询成功';
+                results.data = {
+                    list: result[1],
+                    totalPage: totalPage,
+                    currentPage: page
+                }
+            }
+            client.close();
+            res.json(results)
+        })
+    })
 })
 
 app.listen(3000);
